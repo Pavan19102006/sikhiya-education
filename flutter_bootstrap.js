e4fcf70a431db4f38e5ddc6a6b6307d82ad58270
Boppana Pavan Prasad <boppanapavanprasad@Boppanas-MacBook-Air.local>
2025-09-14 07:40:59 +0530

    Update dashboard: Add Student & Teacher dashboards with progress tracking

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"ddf47dd3ff96dbde6d9c614db0d7f019d7c7a2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "983781897"
+    serviceWorkerVersion: "2890231895"
   }
 });
